1b025b8169dbef56d5ef17b4c2809a46c8a2f85b
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-12-17 21:03:31 +0545

    completed tutorial 2

--- drf/urls.py
+++ drf/urls.py
@@ -25,7 +25,7 @@ router.register(r'users', v.UserViewSet)
 router.register(r'groups', v.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('employees/', views.EmployeeList.as_view()),
